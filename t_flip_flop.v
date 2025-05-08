171107acd076e90530e4879694a1cc6e8de90a23
PawlosAD <156163244+PawlosAD@users.noreply.github.com>
2025-05-08 02:47:50 -0600

    Update t_flip_flop.v

--- t_flip_flop.v
+++ t_flip_flop.v
@@ -1,19 +1,24 @@
+// Implements T type(toggle) flip flop through internally using JK type flip flop.
+// The output toggles when the T input is 1(high) on a rising clock edge in a T flip flop.
+// And it holds its state when T is 0(low)
 module t_flip_flop
   (
-    input t,
-    input clk,
-    output q,
-    output qn
+    input t, // t input that causes output to toggle on rising clock edge when high
+    input clk, // triggers state change on rising edge
+    output q, // output of flip flop
+    output qn // inverse of q
   );
-    wire j, k;
-    assign j = t;
-    assign k = t;
-
+    wire j, k; // internal wires for j and k inputs for JK flip flop
+  // creates t flip flop behavior using jk flip flop
+    assign j = t; // sets j input of jk flip flop to t
+    assign k = t; // sets K input of jk flip flop to t
+  
+// Flip flop toggles state when both J and K are 1, and it holds when 0.
   jk_flip_flop jkff(
-    .j(j),
-    .k(k)
-    .clk(clk),
-    .q(q),
-    .qn(qn)
+    .j(j), // connect t to j input
+    .k(k) // connect to to k input
+    .clk(clk), // connect clock input
+    .q(q), // connect output q
+    .qn(qn) // connect complement Qn
   );
 endmodule
